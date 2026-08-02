c8c3ec097f08ffebad4f1430a25a686bf97164ba
Bella Lu <143304572+bellaa-a@users.noreply.github.com>
2026-08-01 17:18:44 -0700

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        self.res = []
+        self.nums = nums
+        self.dfs([], set())
+        return self.res
+        
+    def dfs(self, subset, seen):
+        for i in range(len(self.nums)):
+            temp = seen.copy()
+            n = self.nums[i]
+            if seen and n in seen:
+                continue
+
+            if len(subset) >= len(self.nums)-1:
+                self.res.append(subset+[n])
+            else:
+                temp.add(n)
+                self.dfs(subset+[n], temp)
